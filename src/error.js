2f385efa5d81104645a9389a67c5efcaa30be0cb
Bart Versluijs <bartv@vedicium.com>
2020-05-28 21:40:53 +0200

    Added httpStatusCode and apiError getters

--- src/error.js
+++ src/error.js
@@ -10,13 +10,36 @@ export default class CustomError extends Error {
 
     // HTTP options
     this.http = {
-      code: 500,
+      statusCode: 500,
     };
 
     // Configure error
     this.configureError(params);
   }
 
+  // Getters
+  get httpStatusCode () {
+    // Get http statusCode from http.statusCode or set 500 as default
+    return (this.http && typeof this.http.statusCode === 'number' ? this.http.statusCode : 500);
+  }
+
+  get apiError () {
+    // Format error to an JSON API response
+    const apiError = {
+      error: (this.type || 'error'),
+      code: this.code,
+      message: this.message,
+    };
+
+    // Add params, if set
+    if (this.params) {
+      apiError.params = this.params;
+    }
+
+    return apiError;
+  }
+
+  // Methods
   configureError (_params = {}) {
     return this;
   }
